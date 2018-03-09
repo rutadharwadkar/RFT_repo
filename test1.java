6ccdd019bc92b2a274e81c18d4a4ec4671d32a3b
rutadharwadkar <35056682+rutadharwadkar@users.noreply.github.com>
2018-03-09 14:21:20 +0530

    Update test1.java

--- test1.java
+++ test1.java
@@ -1,1 +1,12 @@
-System.out.println("Hello world");
+import java.util.*;
+
+public class test1
+{
+public static void main(String[] args)
+{
+  int a = 10;
+  System.out.println(a);
+  System.out.println("Hello world");
+
+}
+}
